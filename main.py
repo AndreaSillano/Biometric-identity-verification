036ff5123be48f2968c72da0519b3a5cf98ef192
AndreaSillano <andreasillano@gmail.com>
2023-06-29 17:26:21 +0200

    Plot Test

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from mlFunc import *
 
 def plotHist(D, L):
+    D = D.transpose()
     D0 = sorted(D[0, L==0])
     D1 = sorted(D[0, L==1])
-    print(D)
-    plt.hist(D0, bins = 20, density=True, ec='black', color="#F77C3F", alpha = 0.5, label="Different Speaker")
-    plt.hist(D1, bins = 20, density=True, ec='black', color="#F77C3F", alpha = 0.5, label="Same Speaker")
+
+    plt.hist(D0, bins = 100, density=True, ec='black', color="#E23A2E", alpha = 0.5, label="Different Speaker")
+    plt.hist(D1, bins = 100, density=True, ec='black', color="#279847", alpha = 0.5, label="Same Speaker")
+
+    plt.legend(loc='upper right')
     plt.show()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    #Load Data & Test Set + Randomize
     DTR,LTR = load("Train.txt")
-    DTR, LTR = randomize(DTR, LTR)
+    #DTR, LTR = randomize(DTR, LTR)
     DTE, LTE = load("Test.txt")
     DTE, LTE = randomize(DTE, LTE)
     plotHist(DTR, LTR)
