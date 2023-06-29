273857c1e15087e8d146f81094125bac14d997be
AndreaSillano <andreasillano@gmail.com>
2023-06-29 17:58:40 +0200

    Histograms Plots

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import matplotlib.pyplot as plt
+
 from mlFunc import *
 
 def plotHist(D, L):
+
     D = D.transpose()
-    D0 = sorted(D[0, L==0])
-    D1 = sorted(D[0, L==1])
 
-    plt.hist(D0, bins = 100, density=True, ec='black', color="#E23A2E", alpha = 0.5, label="Different Speaker")
-    plt.hist(D1, bins = 100, density=True, ec='black', color="#279847", alpha = 0.5, label="Same Speaker")
+    for i in range(0,D.shape[0]):
+        D0 = (D[:, L==0])[i]
+        D1 = (D[:, L==1])[i]
 
-    plt.legend(loc='upper right')
-    plt.show()
+        plt.hist(D0, bins = 100, density=True, ec='black', color="#E23A2E", alpha = 0.5, label="Different Speaker")
+        plt.hist(D1, bins = 100, density=True, ec='black', color="#279847", alpha = 0.5, label="Same Speaker")
+
+        plt.legend(loc='upper right')
+        plt.savefig("./images/hist/hist_" + str(i) + ".png")
+        plt.show()
+
 
 
 if __name__ == "__main__":
@@ -17,6 +24,6 @@ if __name__ == "__main__":
     DTR,LTR = load("Train.txt")
     #DTR, LTR = randomize(DTR, LTR)
     DTE, LTE = load("Test.txt")
-    DTE, LTE = randomize(DTE, LTE)
+    #DTE, LTE = randomize(DTE, LTE)
     plotHist(DTR, LTR)
     
