7f8de9220be3b9325c37f33d331453476ba627ee
AndreaSillano <andreasillano@gmail.com>
2023-07-02 12:03:07 +0200

    evualation?

--- evaluation.py
+++ evaluation.py
@@ -8,59 +8,111 @@ class Evaluation:
     def __init__(self):
         self.MVG = MultivariateGaussianClassifier()
         self.LR = LogisticRegression()
-        self.svmLin = SupportVectorMachine()
+        #self.svmLin = SupportVectorMachine()
+
+
+
 
     def MVG_evaluation(self, DTR, LTR, DTE, LTE, DP, DPE):
-        print("---------------MVG WITHOUT LDA--------------------------")
+        k =5
+        Dtr = numpy.split(DTR.T, k, axis = 1)
+        Ltr = numpy.split(LTR, k)
+        llrMVG = []
+        labelMVG =[]
 
-        self.MVG.setup_MVG(DTR.T, LTR)
+        for i in range(k):
+            Dte = Dtr[i]
+            print(Dte)
+            Lte = Ltr[i]
+            D = []
+            L = []
+            if i == 0:
+                D.append(numpy.hstack(Dtr[i + 1:]))
+                L.append(numpy.hstack(Ltr[i + 1:]))
+            elif i == k - 1:
+                D.append(numpy.hstack(Dtr[:i]))
+                L.append(numpy.hstack(Ltr[:i]))
+            else:
+                D.append(numpy.hstack(Dtr[:i]))
+                D.append(numpy.hstack(Dtr[i + 1:]))
+                L.append(numpy.hstack(Ltr[:i]))
+                L.append(numpy.hstack(Ltr[i + 1:]))
+
+            D = numpy.hstack(D)
+            L = numpy.hstack(L)
+
+
+
+
+
+
+            print("---------------MVG WITHOUT LDA--------------------------")
+            # s = self.MVG.predict_MVG(DTE.T, LTE)
+            # DFC = evaluation(s,LTE, 0.5, 1, 10)
+
+            self.MVG.setup_MVG(numpy.array(D), numpy.array(L))
+            llrMVG = numpy.append(llrMVG,self.MVG.predict_MVG(Dte,Lte))
+            llrMVG = numpy.hstack(llrMVG)
+            labelMVG = numpy.append(labelMVG,Lte,axis = 0)
+            labelMVG = numpy.hstack(labelMVG)
+
+        minDFC = compute_min_DCF(numpy.array(llrMVG),numpy.array(labelMVG), 0.5, 1, 10 )
+        print("MIN DFC", minDFC)
+
+
+
+
+
+        #########################################################
+        #                     DFC on test data
+        #########################################################
         s = self.MVG.predict_MVG(DTE.T, LTE)
-        DFC = evaluation(s,LTE, 0.5, 1, 10)
-        print(DFC)
+        res= compute_act_DCF(s, LTE, 0.5,1, 10, None)
+        print("ACT DFC", res)
+        # print("---------------MVG WITH LDA--------------------------")
+        # self.MVG.setup_MVG(DP, LTR)
+        # s1 = self.MVG.predict_MVG(DPE, LTE)
+        # #DFC1 = evaluation(s1, LTE, 0.5, 1, 10)
 
-        print("---------------MVG WITH LDA--------------------------")
-        self.MVG.setup_MVG(DP, LTR)
-        s1 = self.MVG.predict_MVG(DPE, LTE)
-        DFC1 = evaluation(s1, LTE, 0.5, 1, 10)
-        print(DFC1)
 
-        print("---------------MVG NAIVE BAYES WITHOUT LDA--------------------------")
-        self.MVG.setup_MVG_Naive_Bayes(DTR.T, LTR)
-        s2 = self.MVG.predict_MVG_Naive_Bayes(DTE.T, LTE)
-        DFC2 = evaluation(s2, LTE, 0.5, 1, 10)
-        print(DFC2)
-        print("---------------MVG NAIVE BAYES WITH LDA--------------------------")
-
-        self.MVG.setup_MVG_Naive_Bayes(DP, LTR)
-        s3 = self.MVG.predict_MVG_Naive_Bayes(DPE, LTE)
-        DFC3 = evaluation(s3, LTE, 0.5, 1, 10)
-        print(DFC3)
-        print("---------------MVG TIED COV WITHOUT LDA--------------------------")
-
-        self.MVG.setup_MVG_Tied_Cov(DTR.T, LTR)
-        s4 = self.MVG.predict_MVG_Tied_Cov(DTE.T, LTE)
-        DFC4 = evaluation(s4, LTE, 0.5, 1, 10)
-
-        print(DFC4)
-        print("---------------MVG TIED COV WITH LDA--------------------------")
-
-        self.MVG.setup_MVG_Tied_Cov(DP, LTR)
-        self.MVG.predict_MVG_Tied_Cov(DPE, LTE)
-
-        print("---------------MVG TIED COV + NAIVE WITHOUT LDA--------------------------")
-
-        self.MVG.setup_MVG_Tied_Cov_Naive(DTR.T, LTR)
-        self.MVG.predict_MVG_Tied_Cov_Naive(DTE.T, LTE)
-
-        print("---------------MVG TIED COV + NAIVE WITH LDA--------------------------")
-        self.MVG.setup_MVG_Tied_Cov_Naive(DP, LTR)
-        self.MVG.predict_MVG_Tied_Cov_Naive(DPE, LTE)
-
-        print("---------------LOGISTIC REGRESSION WITHOUT LDA--------------------------")
-        self.LR.setup_Logistic_Regression(DTR.T, LTR, 0.1)
-        self.LR.preditc_Logistic_Regression(DTE.T, LTE, 0.1)
-
-        print("---------------LOGISTIC REGRESSION WITH LDA--------------------------")
-        self.LR.setup_Logistic_Regression(DP, LTR, 0.1)
-        self.LR.preditc_Logistic_Regression(DPE, LTE, 0.1)
+        #
+        # print("---------------MVG NAIVE BAYES WITHOUT LDA--------------------------")
+        # self.MVG.setup_MVG_Naive_Bayes(DTR.T, LTR)
+        # s2 = self.MVG.predict_MVG_Naive_Bayes(DTE.T, LTE)
+        # DFC2 = evaluation(s2, LTE, 0.5, 1, 10)
+        # print(DFC2)
+        # print("---------------MVG NAIVE BAYES WITH LDA--------------------------")
+        #
+        # self.MVG.setup_MVG_Naive_Bayes(DP, LTR)
+        # s3 = self.MVG.predict_MVG_Naive_Bayes(DPE, LTE)
+        # DFC3 = evaluation(s3, LTE, 0.5, 1, 10)
+        # print(DFC3)
+        # print("---------------MVG TIED COV WITHOUT LDA--------------------------")
+        #
+        # self.MVG.setup_MVG_Tied_Cov(DTR.T, LTR)
+        # s4 = self.MVG.predict_MVG_Tied_Cov(DTE.T, LTE)
+        # DFC4 = evaluation(s4, LTE, 0.5, 1, 10)
+        #
+        # print(DFC4)
+        # print("---------------MVG TIED COV WITH LDA--------------------------")
+        #
+        # self.MVG.setup_MVG_Tied_Cov(DP, LTR)
+        # self.MVG.predict_MVG_Tied_Cov(DPE, LTE)
+        #
+        # print("---------------MVG TIED COV + NAIVE WITHOUT LDA--------------------------")
+        #
+        # self.MVG.setup_MVG_Tied_Cov_Naive(DTR.T, LTR)
+        # self.MVG.predict_MVG_Tied_Cov_Naive(DTE.T, LTE)
+        #
+        # print("---------------MVG TIED COV + NAIVE WITH LDA--------------------------")
+        # self.MVG.setup_MVG_Tied_Cov_Naive(DP, LTR)
+        # self.MVG.predict_MVG_Tied_Cov_Naive(DPE, LTE)
+        #
+        # print("---------------LOGISTIC REGRESSION WITHOUT LDA--------------------------")
+        # self.LR.setup_Logistic_Regression(DTR.T, LTR, 0.1)
+        # self.LR.preditc_Logistic_Regression(DTE.T, LTE, 0.1)
+        #
+        # print("---------------LOGISTIC REGRESSION WITH LDA--------------------------")
+        # self.LR.setup_Logistic_Regression(DP, LTR, 0.1)
+        # self.LR.preditc_Logistic_Regression(DPE, LTE, 0.1)
 
--- mlFunc.py
+++ mlFunc.py
@@ -106,39 +106,188 @@ def computeCovDiag(M, muc):
     diagCov = numpy.diag(numpy.diag(cov))
     return (diagCov)
 
-def assign_labels(scores, th):
-    pred = scores>th
-    return numpy.int32(pred)
 
-def DFC(scores, LTE,pi, C_fn, C_fp, th):
-    th = -numpy.log(pi*C_fn/((1-pi)*C_fp))
-    pred_labels = assign_labels(scores, th)
-    confusion_matrix = numpy.zeros((2, 2))
-    confusion_matrix[0, 0] = ((pred_labels == 0) * (LTE == 0)).sum()
-    confusion_matrix[0, 1] = ((pred_labels == 0) * (LTE == 1)).sum()
-    confusion_matrix[1, 0] = ((pred_labels == 1) * (LTE == 0)).sum()
-    confusion_matrix[1, 1] = ((pred_labels == 1) * (LTE == 1)).sum()
+def computeOptimalBayesDecisionBinaryTaskV2THRESHOLD(pi1, Cfn, Cfp, llrs, labels, t):
+    # Now, if the llr is > than the threshold => predicted class is 1
+    # If the llr is <= than the threshold => predicted class is 0
+    predictedLabels = (llrs > t).astype(int)
+    # Compute the confusion matrix
+    m = confusionMatrixV2THRESHOLD(predictedLabels, labels, 2, pi1, Cfn, Cfp)
+    return m
 
-    FNR = confusion_matrix[0, 1] / (confusion_matrix[0, 1] + confusion_matrix[1, 1])
-    FPR = confusion_matrix[1, 0] / (confusion_matrix[0, 0] + confusion_matrix[1, 0])
-    DFC = pi * C_fn * FNR + (1 - pi) * C_fp * FPR
-    return DFC
+def confusionMatrixV2THRESHOLD(pl, LEV, K, pi1, Cfn, Cfp):
+    # Initialize matrix of K x K zeros
+    matrix = numpy.zeros((K, K))
+    # Here we're not talking about costs yet! We're only computing the confusion
+    # matrix which "counts" how many times we get prediction i when the actual
+    # label is j.
+    for i in range(LEV.size):
+        # Update "counter" in proper position
+        matrix[pl[i], LEV[i]] += 1
+    return matrix
+
+def evaluationBinaryTaskV2THRESHOLD(pi1, Cfn, Cfp, confMatrix):
+    # Compute FNR and FPR
+    FNR = confMatrix[0][1]/(confMatrix[0][1]+confMatrix[1][1])
+    FPR = confMatrix[1][0]/(confMatrix[0][0]+confMatrix[1][0])
+    # Compute empirical Bayes risk, that is the cost that we pay due to our
+    # decisions c* for the test data.
+    DCFu = pi1*Cfn*FNR+(1-pi1)*Cfp*FPR
+    return (DCFu, FNR, FPR)
+
+def normalizedEvaluationBinaryTaskV2THRESHOLD(pi1, Cfn, Cfp, DCFu):
+    # Define vector with dummy costs
+    dummyCosts = numpy.array([pi1*Cfn, (1-pi1)*Cfp])
+    # Compute risk for an optimal dummy system
+    index = numpy.argmin(dummyCosts)
+    # Compute normalized DCF
+    DCFn = DCFu/dummyCosts[index]
+    return DCFn
+
+
+def computeOptimalBayesDecisionBinaryTask(pi1, Cfn, Cfp, llrs, labels):
+    # Compute the threshold
+    t = -numpy.log((pi1*Cfn)/((1-pi1)*Cfp))
+    # Now, if the llr is > than the threshold => predicted class is 1
+    # If the llr is <= than the threshold => predicted class is 0
+    predictedLabels = (llrs > t).astype(int)
+    # Compute the confusion matrix
+    m = confusionMatrix(predictedLabels, labels, 2, pi1, Cfn, Cfp)
+    return m
+
+def confusionMatrix(pl, LEV, K, pi1, Cfn, Cfp):
+    # Initialize matrix of K x K zeros
+    matrix = numpy.zeros((K, K))
+    # Here we're not talking about costs yet! We're only computing the confusion
+    # matrix which "counts" how many times we get prediction i when the actual
+    # label is j.
+    for i in range(LEV.size):
+        # Update "counter" in proper position
+        matrix[pl[i], LEV[i]] += 1
+    print("Confusion matrix with prior pi1=%.1f and costs Cfn=%.1f, Cfp=%.1f:" %
+          (pi1, Cfn, Cfp))
+    print(matrix)
+    return matrix
+
+def evaluationBinaryTask(pi1, Cfn, Cfp, confMatrix):
+    # Compute FNR and FPR
+    FNR = confMatrix[0][1]/(confMatrix[0][1]+confMatrix[1][1])
+    FPR = confMatrix[1][0]/(confMatrix[0][0]+confMatrix[1][0])
+    # Compute empirical Bayes risk, that is the cost that we pay due to our
+    # decisions c* for the test data.
+    DCFu = pi1*Cfn*FNR+(1-pi1)*Cfp*FPR
+    print("Unnormalized detection cost function with prior pi1=%.1f and costs Cfn=%.1f, Cfp=%.1f: %.3f" %
+          (pi1, Cfn, Cfp, DCFu))
+    return DCFu
+
+def normalizedEvaluationBinaryTask(pi1, Cfn, Cfp, DCFu):
+    # Define vector with dummy costs
+    dummyCosts = numpy.array([pi1*Cfn, (1-pi1)*Cfp])
+    # Compute risk for an optimal dummy system
+    index = numpy.argmin(dummyCosts)
+    # Compute normalized DCF
+    DCFn = DCFu/dummyCosts[index]
+    print("Normalized detection cost function with prior pi1=%.1f and costs Cfn=%.1f, Cfp=%.1f: %.3f" %
+          (pi1, Cfn, Cfp, DCFn))
+    return
 
 def evaluation(scores, LTE, pi,C_fn, C_fp):
 
-    ths = numpy.array(scores)
-    ths.sort()
-    print("Prima",ths)
     DFCList =[]
-    t = numpy.concatenate([numpy.array([-numpy.inf]), ths, numpy.array([numpy.inf])])
-
-    print("Dopo",t)
-    for th in t:
-        DFCList.append(DFC(scores,LTE,pi,C_fn,C_fp, th))
-
-    return min(DFCList)
 
 
+    m1 = computeOptimalBayesDecisionBinaryTask(pi, C_fn, C_fp, scores, LTE)
+    DCFu1 = evaluationBinaryTask(pi, C_fn, C_fp, m1)
+    normalizedEvaluationBinaryTask(pi, C_fn, C_fp, DCFu1)
+
+    #for t in th:
+    #    m1 = computeOptimalBayesDecisionBinaryTaskV2THRESHOLD(
+    #        pi, C_fn, C_fp, scores, LTE, t)
+    #    (DCFu1, _, _) = evaluationBinaryTaskV2THRESHOLD(pi, C_fn, C_fp, m1)
+    #    DFCList.append(normalizedEvaluationBinaryTaskV2THRESHOLD(pi, C_fn, C_fp, DCFu1))
+
+    return DCFu1
+
+def compute_min_DCF1(scores, pi,C_fn, C_fp, LTR):
+    th = numpy.sort(scores)
+    DFCList =[]
+    for t in th:
+        m1 = computeOptimalBayesDecisionBinaryTaskV2THRESHOLD(
+            pi, C_fn, C_fp, scores, LTR, t)
+        (DCFu1, _, _) = evaluationBinaryTaskV2THRESHOLD(pi, C_fn, C_fp, m1)
+        DFCList.append(normalizedEvaluationBinaryTaskV2THRESHOLD(pi, C_fn, C_fp, DCFu1))
+
+    index = np.argmin(DFCList)
+    print("Min DCF with prior pi1=%.1f and costs Cfn=%.1f, Cfp=%.1f: " %
+          (0.5, 1, 1))
+    print(DCFarr1[index])
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def confusion_matrix(Lpred, LTE, k=2):
+    # k = number of classes
+    conf = numpy.zeros((k, k))
+    for i in range(k):
+        for j in range(k):
+            conf[i][j] = ((Lpred == i) * (LTE == j)).sum()
+    return conf
+
+
+def assign_labels(scores, pi, Cfn, Cfp, th=None):
+    if th is None:
+        th = -numpy.log(pi * Cfn) + numpy.log((1 - pi) * Cfp)
+    P = scores > th
+    return numpy.int32(P)
+
+
+def confusion_matrix_binary(Lpred, LTE):
+    C = numpy.zeros((2, 2))
+    C[0, 0] = ((Lpred == 0) * (LTE == 0)).sum()
+    C[0, 1] = ((Lpred == 0) * (LTE == 1)).sum()
+    C[1, 0] = ((Lpred == 1) * (LTE == 0)).sum()
+    C[1, 1] = ((Lpred == 1) * (LTE == 1)).sum()
+    return C
+
+
+def compute_emp_Bayes_binary(CM, pi, Cfn, Cfp):
+    fnr = CM[0, 1] / (CM[0, 1] + CM[1, 1])
+    fpr = CM[1, 0] / (CM[0, 0] + CM[1, 0])
+    return pi * Cfn * fnr + (1 - pi) * Cfp * fpr
+
+
+def compute_normalized_emp_Bayes(CM, pi, Cfn, Cfp):
+    empBayes = compute_emp_Bayes_binary(CM, pi, Cfn, Cfp)
+    return empBayes / min(pi * Cfn, (1 - pi) * Cfp)
+
+
+def compute_act_DCF(scores, labels, pi, Cfn, Cfp, th=None):
+    Pred = assign_labels(scores, pi, Cfn, Cfp, th=th)
+    CM = confusion_matrix_binary(Pred, labels)
+    return compute_normalized_emp_Bayes(CM, pi, Cfn, Cfp)
+
+
+def compute_min_DCF(scores, labels, pi, Cfn, Cfp):
+    t = numpy.array(scores)
+    t.sort()
+    dcfList = []
+    for _th in t:
+        dcfList.append(compute_act_DCF(scores, labels, pi, Cfn, Cfp, th=_th))
+    return numpy.array(dcfList).min()
